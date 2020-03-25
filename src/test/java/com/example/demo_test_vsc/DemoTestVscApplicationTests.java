e0675eb9ac8b53df18e057701e6b2a5def4e6d56
Joe <1527008697@qq.com>
2020-03-25 15:26:30 +0800

    Initial commit

---
+++ src/test/java/com/example/demo_test_vsc/DemoTestVscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_test_vsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestVscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
